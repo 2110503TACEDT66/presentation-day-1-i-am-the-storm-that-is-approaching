f48b3b993784ba3ca8145957147145e8b9188fee
Api Phoom <apiphoom23@gmail.com>
2024-02-29 20:20:01 +0700

    feat : Enhance Booking Query Logic for Role-Based Access

--- routes/companies.js
+++ routes/companies.js
@@ -17,12 +17,12 @@ const {protect, authorize} = require('../middleware/auth');
 //Re-route into other resource routers
 router.use('/:companyId/bookings/', bookingRouter);
 
-router.route("/").get(getCompanies).post(protect, /*authorize('admin'),*/ createCompany);
+router.route("/").get(getCompanies).post(protect, authorize('admin'), createCompany);
 
 router
   .route("/:id")
   .get(getCompany)
-  .put(protect, /*authorize('admin'), */updateCompany)
-  .delete(protect, /*authorize('admin'), */deleteCompany);
+  .put(protect, authorize('admin'), updateCompany)
+  .delete(protect, authorize('admin'), deleteCompany);
 
 module.exports = router;
